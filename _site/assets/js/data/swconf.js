00e5b7b5556909a32e8da68f512c9e9a75fd13cc
minjaemong <03lmj0710@naver.com>
2024-08-28 10:39:12 +0900

    1

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724808396',resources: [
+    cacheName: 'chirpy-1724808556',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
